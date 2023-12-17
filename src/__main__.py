0a7ec56bc5bb5a13972ab90149e9d33cf6a6880f
kingsmanvn1112 <nguyenhuuvuong11122002@gmail.com>
2023-12-17 18:37:36 +0700

    update

--- src/__main__.py
+++ src/__main__.py
@@ -2,9 +2,10 @@ import asyncio
 import aiohttp
 
 from loguru import logger
-from src import cloudflare, convert ,utils
+from src import cloudflare, convert, utils
 
-class CloudflareManager:    
+
+class CloudflareManager:
     def __init__(
         self, adlist_name: str, adlist_urls: list[str], whitelist_urls: list[str]
     ):
@@ -15,7 +16,7 @@ class CloudflareManager:
 
     async def run(self):
 
-        # Download block and white content 
+        # Download block and white content
         async with aiohttp.ClientSession() as session:
             block_content = "".join(
                 await asyncio.gather(
@@ -33,7 +34,7 @@ class CloudflareManager:
                     ]
                 )
             )
-            
+
         # Add dynamic_blacklist
         with open("./lists/dynamic_blacklist.txt", "r") as block_file:
             block_content += block_file.read()
@@ -41,17 +42,13 @@ class CloudflareManager:
         # Add dynamic_whitelist
         with open("./lists/dynamic_whitelist.txt", "r") as white_file:
             white_content += white_file.read()
-                        
+
         domains = convert.convert_to_domain_list(block_content, white_content)
-        
+
         # check if number of domains exceeds the limit
         if len(domains) == 0:
-            logger.warning("No domains found in the adlist file. Exiting script.")
-            return 
-        
-        # stop script if the number of final domains exceeds the limit
-        if len(domains) > 300000:
-            logger.warning("The number of final domains exceeds the limit. Exiting script.")
+            logger.warning(
+                "No domains found in the adlist file. Exiting script.")
             return
 
         # check if the list is already in Cloudflare
@@ -67,13 +64,15 @@ class CloudflareManager:
             if len(cf_policies) == 0:
                 logger.info("No firewall policy found, creating new policy")
                 cf_policies = await cloudflare.create_gateway_policy(
-                    f"{self.name_prefix} Block Ads", [l["id"] for l in cf_lists]
+                    f"{self.name_prefix} Block Ads", [
+                        l["id"] for l in cf_lists]
                 )
             else:
-                logger.warning("Firewall policy already exists, exiting script")
+                logger.warning(
+                    "Firewall policy already exists, exiting script")
                 return
 
-            return 
+            return
 
         # Delete existing policy created by script
         policy_prefix = f"{self.name_prefix} Block Ads"
@@ -82,11 +81,12 @@ class CloudflareManager:
             await cloudflare.delete_gateway_policy(policy["id"])
         logger.info(f"Deleted gateway policies")
 
-        # Delete old lists on Cloudflare 
+        # Delete old lists on Cloudflare
         delete_list_tasks = []
         for l in cf_lists:
             logger.info(f"Deleting list {l['name']} - ID:{l['id']} ")
-            delete_list_tasks.append(cloudflare.delete_list(l["name"], l["id"]))
+            delete_list_tasks.append(
+                cloudflare.delete_list(l["name"], l["id"]))
         await asyncio.gather(*delete_list_tasks)
 
         # Start creating new lists and firewall policy concurrently
@@ -95,7 +95,7 @@ class CloudflareManager:
             list_name = f"{self.name_prefix} {i + 1:03d}"
             logger.info(f"Creating list {list_name}")
             create_list_tasks.append(cloudflare.create_list(list_name, chunk))
-    
+
         cf_lists = await asyncio.gather(*create_list_tasks)
 
         cf_policies = await cloudflare.get_firewall_policies(self.name_prefix)
@@ -133,15 +133,18 @@ class CloudflareManager:
         delete_list_tasks = []
         for l in cf_lists:
             logger.info(f"Deleting list {l['name']} - ID:{l['id']} ")
-            delete_list_tasks.append(cloudflare.delete_list(l["name"], l["id"]))
+            delete_list_tasks.append(
+                cloudflare.delete_list(l["name"], l["id"]))
         await asyncio.gather(*delete_list_tasks)
         logger.info("Deletion completed")
+
 
 async def main():
     adlist_urls = utils.read_urls_from_file("./lists/adlist.ini")
     whitelist_urls = utils.read_urls_from_file("./lists/whitelist.ini")
     adlist_name = "DNS-Filters"
-    cloudflaremanager = CloudflareManager(adlist_name, adlist_urls, whitelist_urls)
+    cloudflaremanager = CloudflareManager(
+        adlist_name, adlist_urls, whitelist_urls)
     # await cloudflaremanager.leave()  # Leave script
     await cloudflaremanager.run()
 
